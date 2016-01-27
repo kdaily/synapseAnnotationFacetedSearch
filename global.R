22e82b04a1dda3521cc369a4a06574d5cdcfd525
kdaily <kdaily@users.noreply.github.com>
2016-01-27 15:55:26 -0800

    change cols

--- global.R
+++ global.R
@@ -11,14 +11,17 @@ synapseLogin()
 source("lib.R")
 
 projectId <- "syn2787333"
-colsUsed <- c("study","group","platform","dataType","fileType")
+colsUsed <- c("study", "group", "assay", "assayTarget",
+              "organism", "disease",
+              "tissueType", "cellType", "treatmentType", 
+              "dataType", "dataSubType", "fileType")
 
-# queryString <- sprintf("select id,%s from file where projectId=='%s'", 
-#                        paste(colsToUse, collapse=","),
-#                        projectId)
-# dfData <- synQuery(queryString, 250)$collectAll()
-# colnames(dfData) <- gsub('file\\.', '', colnames(dfData))
-# save(dfData, file="dfData.RData")
+queryString <- sprintf("select id,%s from file where projectId=='%s'",
+                       paste(colsUsed, collapse=","),
+                       projectId)
+dfData <- synQuery(queryString, 450)$collectAll()
+colnames(dfData) <- gsub('file\\.', '', colnames(dfData))
+save(dfData, file="dfData.RData")
 load("dfData.RData")
 dfData <- dfData %>%
   mutate(id=sprintf("<a href='https://www.synapse.org/#!Synapse:%s'>%s</a>", id, id))
