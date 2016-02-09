68dc46340718378a831d2f6a28a2ea1477d8f0ff
kdaily <kdaily@users.noreply.github.com>
2016-02-08 21:44:47 -0800

    force install dependencies

--- global.R
+++ global.R
@@ -1,8 +1,22 @@
-library(shiny)
-library(DT)
-library(plyr)
-library(dplyr)
-library(reshape2)
+options(repos = "https://cran.rstudio.com/")
+
+libs <- c("shiny", "DT", "plyr", "dplyr", "reshape2")
+
+available <- suppressWarnings(sapply(libs, require, character.only=TRUE))
+inst.libs <- libs[available == FALSE]
+
+if(length(inst.libs) != 0) {
+  install.packages(inst.libs, dependencies = TRUE)
+}
+
+suppressWarnings(sapply(libs, require, character.only=TRUE))
+
+synapseAvailable <- suppressWarnings(require("synapseClient", character.only=TRUE))
+
+if (!synapseAvailable) {
+  source('http://depot.sagebase.org/CRAN.R')
+  pkgInstall(c("synapseClient"))
+}
 
 library(synapseClient)
 synapseLogin()
